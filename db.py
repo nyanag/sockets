b09dba5a85691fe532f9ac401998ba00ff91a96b
Ananya <ananyaganesh01@gmail.com>
2020-06-05 22:54:29 +0530

    Sockets work

--- db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('AA_db.sqlite')
-cur = conn.cursor()
-cur.execute('INSERT INTO experiments (name, description) values ("Aquiles", "My experiment description")')
-conn.commit()
-
-
-conn.close()
